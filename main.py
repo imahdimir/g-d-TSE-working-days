7186540ddf6512424e120203b580a2ca81d93efb
imahdimir <imahdimir@gmail.com>
2022-09-09 20:51:58 +0430

    create

---
+++ main.py
@@ -0,0 +1,37 @@
+"""
+
+    """
+
+import pandas as pd
+from githubdata import GithubData
+
+
+
+
+def main():
+    pass
+
+    ##
+
+    ##
+
+    ##
+
+##
+if __name__ == "__main__":
+    main()
+
+##
+# noinspection PyUnreachableCode
+if False:
+    pass
+
+    ##
+
+
+    ##
+
+
+    ##
+
+##
